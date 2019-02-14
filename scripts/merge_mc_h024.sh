92153a19fa450e5cae9a49744d30b79c24ba1f61
Ivan Pogrebnyak <ivan.pogrebnyak@cern.ch>
2019-02-14 01:51:44 +0100

    h024 merge mc

--- scripts/merge_mc_h024.sh
+++ scripts/merge_mc_h024.sh
@@ -1,30 +1,35 @@
 #!/bin/bash
 
-runs=($(sed -rn 's,.*data([0-9]{2})/.*_([0-9]+)ipb.*,\1 \2,p' \
-  $(sed -r 's,(.*/).*,\1data_*_all.sh,' <<< "$0") \
+dir=${0%/*}
+
+runs=($(
+sed -rn 's,.*data([0-9]{2})/.*_([0-9]+)ipb.*,\1 \2,p' $dir/data_*_all.sh \
 | awk '
 BEGIN{
   ds[15] = "a"
   ds[16] = "a"
   ds[17] = "d"
   ds[18] = "e"
-} { a[ds[$1]]+=$2; t+=$2 } END{ for (i in a) print "mc16" i ":" a[i]/t }'))
+} { a[ds[$1]]+=$2; t+=$2 } END{ for (i in a) print "mc16" i ":" a[i]/t }'
+))
 
-# runs=(mc16a:0.451721 mc16d:0.548279 mc16e:1)
+printf '%s\n' "${runs[@]}"
 
-dir=out/h024
+dir=$(readlink -f "$dir/../out/h024")
 out=$dir/mc16_merged
 
 mkdir -p $out
 
 for name in $(
-  find $(sed -r "s,([^ ]*):[^ ]+,$dir/\\1,g" <<< ${runs[@]}) \
-  | sed -rn 's,.*/(.*)\.root$,\1,p' \
+  find $(sed -r "s,([^ ]*):[^ ]+,$dir/\1,g" <<< ${runs[@]}) \
+  | sed -rn 's,.*/(.*\.root)$,\1,p' \
   | sort -u
 )
 do
-  # echo $name
-  echo \
-  ./bin/merge_mc ${runs[@]/#/$dir\/} -o $out/$name
+  echo ${name%.root}
+  # echo \
+  ./bin/merge_mc \
+  $(sed -r "s,([^ ]*)(:[^ ]*),$dir/\1/$name\2,g" <<< ${runs[@]}) \
+  -o $out/$name
 done
 
